c77ee5bb5ab0eea9befd3b58ee43c2245da4bd7d
tothemax314 <tothemax31415@gmail.com>
2021-05-18 00:14:50 -0400

    Made countdown go brrr

--- js/rocket.js
+++ js/rocket.js
@@ -1,9 +1,38 @@
-var changeState = function (state){
-document.body.className = "bodystate"+state;
+var timer = null;
+var countdownNumber = 10;
+
+var changeState = function (state) {
+document.body.className = "body-state"+state;
+clearInterval (timer);
+countdownNumber = 10;
+document.getElementById('countdown').innerHTML = 
+		countdownNumber;
 	
 	if (state == 2){
-	document.getElementById('countdown').innerHTML = 9;	
+		timer = setInterval(function () {
+			countdownNumber = countdownNumber
+		      - 1;
+		document.getElementById('countdown').innerHTML = 
+		countdownNumber; 
+		      if (countdownNumber <= -1) {
+		      	changeState(3);
+		      };
+        }, 500);
+      } else if (state == 3){
+          var success = setTimeout (function ()
+          {
+             var randomNumber = Math.round (Math.random()*10);
+            
+             console.log('randomNumber:', randomNumber)
+              // > 5 means success
+             if (randomNumber >= 5) { changeState(4);
+             	//do something
+             } else {
+             	changeState (5);
+             }
+
+          }, 2000);
+       };
+       
 	}
 
-
-}
